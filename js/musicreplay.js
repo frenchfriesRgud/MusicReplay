6e148016537857d9f4285e76311fb13f856f72f2
anthony <thestonesserver@gmail.com>
2020-12-03 22:59:51 +0000

    PWA Updates

--- js/musicreplay.js
+++ js/musicreplay.js
@@ -1,15 +1,38 @@
 // Music Replay 2020
 // Created by Anthony.media
 // 2020-12-03
+// https://musicreplay.frenchfriesrgud.repl.co/?duration=777&genre=rocknroll&artwork=afasfdasfd&a1=z&a2=z&a3=z&a4=z&a5=z&s1=x&s2=x&s3=x&s4=x&s5=x#share
 console.log('/js/musicreplay.js connected');
-if (window.location.hash === '#share') {
-	console.log('render the sharable view');
-} else {
-	console.log('Render the build view');
-}
 const app = {
 	vendor: 'apple',
 	state: '',
+    router: {
+        params: null,
+        getParams: () => {
+            if(window.location.search){
+                app.router.params = new URLSearchParams(window.location.search);
+                return;
+            } else {
+                return console.warn('No search params include in URL');
+            }
+        },
+        buildFromParams: () => {
+            //duration
+            form.data.duration = app.router.params.get('duration');
+            //genre
+            form.data.genre = app.router.params.get('genre');
+            // artwork
+            form.data.artwork = app.router.params.get('artwork');
+            // loop
+            for(let index = 1; index <= 5; index++){
+                console.log(index);
+                // artists
+                form.data.artists.push(app.router.params.get(`a${index}`));
+                // songs
+                form.data.songs.push(app.router.params.get(`s${index}`));
+            }
+        }
+    },
 	generator: {
 		el: document.querySelector('.generate'),
 		show: () => {
@@ -35,7 +58,9 @@ const app = {
 			app.replay.el.classList.add('hide');
 		},
 		build: (source) => {
-			source.getData();
+            if(app.state === "build"){
+			    source.getData();
+            }
 
 			// hide the build view
 			app.generator.hide();
@@ -46,7 +71,7 @@ const app = {
 			// update the data
 			app.replay.display.duration.innerHTML = (source.data.duration * 60).toLocaleString();
 			app.replay.display.genre.innerHTML = source.data.genre;
-			app.replay.display.artwork.src = source.data.artwork;
+			app.replay.display.artwork.style.background = `url('${source.data.artwork}')`;
 			// loop through the billboard data
 			app.replay.display.artists.forEach((artist, index) => {
 				artist.innerHTML = source.data.artists[index];
@@ -98,6 +123,18 @@ const form = {
 	}
 };
 
+// On load 
+if (window.location.hash === '#share') {
+	console.log('render the sharable view');
+    app.state = 'share';
+    app.router.getParams();
+    app.router.buildFromParams();
+    app.replay.build(form);
+} else {
+	console.log('Render the build view');
+    app.state = 'build';
+}
+
 // Event Listeners
 
 tutorial.dismissBtn.addEventListener('click', () => {
@@ -110,3 +147,14 @@ form.el.addEventListener('submit', (event) => {
 	app.replay.build(form);
 	console.log('form submitted');
 });
+
+form.input.artwork.addEventListener('input', event => {
+    document.querySelector('.url-preview').classList.remove('hide');
+    try {
+        let url = `url('${form.input.artwork.value}')`;
+        document.querySelector('.preview').style.background = url;
+    } catch(err){
+        console.log(err);
+    }
+    console.log('artwork changed');
+    });
